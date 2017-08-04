13cdb8089f2e4c06641b4814b5f25f5f51c7b74d
Lukasz Olender <lukasz.olender@euvic.pl>
2017-08-04 22:25:55 +0200

    Publishing to gh-pages (publish.sh)

--- publish.sh
+++ publish.sh
@@ -1,12 +1,6 @@
 
 #!/bin/sh
 
-if [[ $(git status -s) ]]
-then
-    echo "The working directory is dirty. Please commit any pending changes."
-    exit 1;
-fi
-
 echo "Deleting old publication"
 rm -rf public
 mkdir public
